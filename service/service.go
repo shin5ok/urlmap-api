e65ac6de5ed347117061a5b6daede2cdee6ba944
shin5ok <shin5ok@55mp.com>
2021-02-16 00:39:32 +0900

    add service

---
+++ service/service.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+	"errors"
+	pb "urlmap-api/pb"
+)
+
+type Redirection struct{}
+
+func (s *Redirection) GetInfo(ctx context.Context, org *pb.OrgUrl) (*pb.RedirectData, error) {
+	redirectdata := &pb.RedirectData{}
+	if true {
+		return redirectdata, nil
+	}
+	return nil, errors.New("erro")
+}
+
+func (s *Redirection) SetInfo(ctx context.Context, org *pb.RedirectData) (*pb.OrgUrl, error) {
+	// just stub for a test
+	if true {
+		return &pb.OrgUrl{}, nil
+	}
+	return nil, errors.New("erro")
+}
