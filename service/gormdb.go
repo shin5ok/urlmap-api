f4c74cda9c2c03b3e329fce48f627863724ddcfb
shin5ok <shin5ok@55mp.com>
2021-07-11 00:38:20 +0900

    remove and modify

--- service/gormdb.go
+++ service/gormdb.go
@@ -3,6 +3,7 @@ package service
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -18,7 +19,6 @@ func sqlConnect() (database *gorm.DB, err error) {
 	PROTOCOL := fmt.Sprintf("tcp(%s:3306)", HOST)
 
 	CONNECT := DBUSER + ":" + DBPASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-	log.Println(CONNECT)
 	return gorm.Open(DBMS, CONNECT)
 }
 
@@ -38,7 +38,29 @@ func main() {
 	}
 	db.Create(&Redirects{
 		User:         "tako",
-		RedirectPath: "shuya",
+		RedirectPath: "ika",
 		Org:          "https://www.example.tv",
+		Active:       1,
 	})
+	results := []*Redirects{}
+	error := db.Find(&results).Error
+	if error != nil || len(results) == 0 {
+		return
+	}
+	for i, r := range results {
+		fmt.Printf("%d: %s, %s, %s, %d\n", i, r.User, r.RedirectPath, r.Org, r.Active)
+
+	}
+}
+
+func genRand(i int) string {
+	src := []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	x := make([]rune, i)
+	for n := range x {
+		x[n] = src[rand.Intn(len(src))]
+	}
+	rand := string(x)
+	fmt.Println(rand)
+	return rand
 }
