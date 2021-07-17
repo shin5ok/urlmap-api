66b192150422118ac6189c9de9e4880f654c22b3
shin5ok <shin5ok@55mp.com>
2021-07-17 19:35:45 +0900

    delete some print for debug

--- service/service.go
+++ service/service.go
@@ -68,7 +68,7 @@ func (s *Redirection) GetInfoByUser(ctx context.Context, user *pb.User) (*pb.Arr
 	}
 
 	pbResults.Redirects = resultSlice
-	log.Print(pbResults)
+	// log.Print(pbResults)
 	return pbResults, nil
 }
 
@@ -86,7 +86,7 @@ func (s *Redirection) GetOrgByPath(ctx context.Context, path *pb.RedirectPath) (
 		log.Println(status.Error)
 		return &pb.OrgUrl{}, status.Error
 	}
-	log.Println(result)
+	// log.Println(result)
 
 	return &pb.OrgUrl{Org: result.Org}, nil
 
