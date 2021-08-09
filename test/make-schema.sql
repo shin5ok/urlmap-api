718ebbbac756d9791993dc8be0db997b704013bf
shin5ok <shin5ok@55mp.com>
2021-08-09 22:24:42 +0900

    change schema to implement notification feature

--- test/make-schema.sql
+++ test/make-schema.sql
@@ -1,7 +1,13 @@
+create table if not exists `users`
+(
+    `username` varchar(64) PRIMARY KEY,
+    `notify_to` varchar(128)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 create table IF not exists `redirects`
 (
- `user`             VARCHAR(20),
- `redirect_path`	VARCHAR(64),
+ `user`             VARCHAR(64),
+ `redirect_path`	VARCHAR(128),
  `org`			VARCHAR(10240),
  `host`			VARCHAR(128),
  `comment`		TEXT,
@@ -11,5 +17,5 @@ create table IF not exists `redirects`
  `created_at`       Datetime DEFAULT NULL,
  `updated_at`       Datetime DEFAULT NULL,
  `deleted_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`redirect_path`)
+  constraint `user_const` FOREIGN KEY (`user`) REFERENCES users(`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
