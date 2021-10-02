b0663edd21b892e02c05e1e376cdf0abad11c27a
Shingo <kawanos@google.com>
2021-10-02 14:34:11 +0900

    test with grpcurl

---
+++ client/grpcurl.sh
@@ -0,0 +1,3 @@
+grpcurl -plaintext -d '{"path": "foo"}' 127.0.0.1:8080 urlmap.Redirection.GetOrgByPath
+grpcurl -plaintext -d '{"user": "tako", "notify_to":"slack"}' 127.0.0.1:8080 urlmap.Redirection.SetUser
+grpcurl -plaintext -d '{"user": "tako"}' 127.0.0.1:8080 urlmap.Redirection.GetInfoByUser
