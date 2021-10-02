741535217b6070a136fad121151002646b02a40e
Shingo <kawanos@google.com>
2021-10-02 14:39:39 +0900

    delete

--- utils/clouddeploy.sh
+++
@@ -1,5 +0,0 @@
-PROJECT=$(gcloud config get-value project)
-# Create each stages and a pipeline
-gcloud beta deploy apply --file clouddeploy.yaml --region=us-central1 --project=$PROJECT
-# Show the list of deploy targets
-gcloud beta deploy targets list --region=us-central1 --project=$PROJECT
