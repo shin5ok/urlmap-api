4b8224a0b655da69f8f32f03e58d693e1d66e08d
Shingo <kawanos@google.com>
2021-10-24 00:49:10 +0900

    modify

--- client/grpc_cli.sh
+++ client/grpc_cli.sh
@@ -1,3 +1,4 @@
 grpc_cli call localhost:8080 urlmap.Redirection.GetOrgByPath "path: 'tako'"
 grpc_cli call localhost:8080 urlmap.Redirection.SetUser "user: 'tako',notify_to: 'slack'"
 grpc_cli call localhost:8080 urlmap.Redirection.GetInfoByUser "user: 'tako'"
+grpc_cli call localhost:8080 urlmap.Redirection.ListUsers ""
