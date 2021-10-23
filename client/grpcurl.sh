d36b96abb034885bedeb863ae9706e8db792d60a
Shingo <kawanos@google.com>
2021-10-24 01:34:01 +0900

    it looks like grpcurl is the better tool than grpc_cli

--- client/grpcurl.sh
+++ client/grpcurl.sh
@@ -1,3 +1,4 @@
 grpcurl -plaintext -d '{"path": "foo"}' 127.0.0.1:8080 urlmap.Redirection.GetOrgByPath
 grpcurl -plaintext -d '{"user": "tako", "notify_to":"slack"}' 127.0.0.1:8080 urlmap.Redirection.SetUser
 grpcurl -plaintext -d '{"user": "tako"}' 127.0.0.1:8080 urlmap.Redirection.GetInfoByUser
+grpcurl -plaintext -d '' 127.0.0.1:8080 urlmap.Redirection.ListUsers
