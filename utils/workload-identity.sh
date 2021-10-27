0c70e5d36a27a9b2a9865bb8a5dab1f2001c61f3
Shingo <kawanos@google.com>
2021-10-27 17:11:28 +0900

    to set workload identity

---
+++ utils/workload-identity.sh
@@ -0,0 +1,4 @@
+kubectl create serviceaccount --namespace urlmap urlmap-api
+gcloud iam service-accounts create urlmap-api
+gcloud projects add-iam-policy-binding $PROJECT     --member "serviceAccount:urlmap-api@$PROJECT.iam.gserviceaccount.com"     --role "roles/secretmanager.secretAccessor"
+gcloud iam service-accounts add-iam-policy-binding     --role roles/iam.workloadIdentityUser     --member "serviceAccount:$PROJECT.svc.id.goog[urlmap/urlmap-api]"     urlmap-api@$PROJECT.iam.gserviceaccount.com
