acaadc0a44d28977f39ef5f66a403bd25b6f49cc
Shingo <kawanos@google.com>
2021-11-28 03:18:53 +0900

    just a test

--- utils/create-secret.sh
+++ utils/create-secret.sh
@@ -1,2 +1,2 @@
 gcloud services enable secretmanager.googleapis.com
-echo $GITHUB_ARGOCD | gcloud secrets create myrepo --data-file=- --locations=us-central1 --replication-policy=user-managed
+echo -n $GITHUB_ARGOCD | gcloud secrets create myrepo --data-file=- --locations=us-central1 --replication-policy=user-managed
