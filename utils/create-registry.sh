376911dd508ffd2333e7367b8bdb6bcf826cf826
Shingo <kawanos@google.com>
2021-11-28 03:41:07 +0900

    add the priv to access artiafct registry to GKE nodes

--- utils/create-registry.sh
+++ utils/create-registry.sh
@@ -1,1 +1,5 @@
 gcloud beta artifacts repositories create myrepo --location=us-central1 --repository-format=docker
+gcloud artifacts repositories add-iam-policy-binding myrepo \
+    --location=us-central1 \
+    --member=serviceAccount:$(gcloud projects describe $PROJECT --format=json | jq .projectNumber -r)-compute@developer.gserviceaccount.com \
+    --role="roles/artifactregistry.reader"
