3a4956f9c33567459187b8b451a3312d2444346d
Shingo <kawanos@google.com>
2021-12-16 22:47:53 +0900

    integrate files into one because of dup of purpose

--- utils/create-secret.sh
+++
@@ -1,2 +0,0 @@
-gcloud services enable secretmanager.googleapis.com
-echo -n $GITHUB_ARGOCD | gcloud secrets create myrepo --data-file=- --locations=us-central1 --replication-policy=user-managed
--- utils/create-secrets.sh
+++ utils/create-secrets.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+gcloud services enable secretmanager.googleapis.com
+
+echo -n $GITHUB_ARGOCD | gcloud secrets create myrepo --data-file=- --locations=us-central1 --replication-policy=user-managed
+
 echo -n $DBUSER | gcloud secrets create DBUSER --data-file=-
 echo -n $DBPASS | gcloud secrets create DBPASS --data-file=-
 echo -n $DBNAME | gcloud secrets create DBNAME --data-file=-
 echo -n $DBHOST | gcloud secrets create DBHOST --data-file=-
-
