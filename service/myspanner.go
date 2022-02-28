67bc010400326d217de79aa76bff1258a7472e87
Shingo <kawanos@google.com>
2022-02-28 23:05:57 +0900

    implement methods of interfaces with factory

--- service/myspanner.go
+++ service/myspanner.go
@@ -2,16 +2,35 @@ package service
 
 import (
 	"context"
+	"errors"
 	"io"
 
-	"cloud.google.com/go/spanner"
 	database "cloud.google.com/go/spanner/admin/database/apiv1"
 )
 
+type spanner struct{}
+
 type MyDB interface {
 	Put(string) error
-	Get(string) []string
-	List() []string
+	Get(string) (*[]string, error)
+	List() (*[]string, error)
+}
+
+func New() *spanner {
+	return &spanner{}
+
+}
+
+func (s *spanner) Put() error {
+	return errors.New("")
+}
+
+func (s *spanner) Get() (*[]string, error) {
+	return &[]string{}, errors.New("")
+}
+
+func (s *spanner) List() (*[]string, error) {
+	return &[]string{}, errors.New("")
 }
 
 func createClients(w io.Writer, db string) error {
