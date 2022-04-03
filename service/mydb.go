c43b64512765622d2ca65e560e223742c9dffe22
Shingo <kawanos@google.com>
2022-04-04 00:27:23 +0900

    define "redirects" table name and columns

--- service/mydb.go
+++ service/mydb.go
@@ -1,7 +1,11 @@
 package service
 
 type MyDB interface {
-	Put(params *[]string) error
-	Get(query string) (*[]string, error)
-	List() (*[]string, error)
+	Put(params []interface{}) error
+	Get(query string) ([]string, error)
+	List() ([]string, error)
 }
+
+var RedirectTableName = "redirects"
+
+var RedirectTableColumn = []string{"user", "redirect_path", "org", "host", "comment", "active", "begin_at", "end_at", "created_at", "update_at", "deleted_at"}
