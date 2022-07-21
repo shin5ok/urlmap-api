e07121d2b76674ab7a05435e4df7c4050ac0fc71
shin5ok <shin5ok@55mp.com>
2022-07-22 01:18:17 +0900

    add some tests

--- service/service_test.go
+++ service/service_test.go
@@ -1,8 +1,67 @@
 package service
 
 import (
+	"context"
 	"testing"
+
+	"github.com/golang/mock/gomock"
+	// gomock "github.com/golang/mock/gomock"
+	mock_urlmap "github.com/shin5ok/urlmap-api/mock_urlmap"
+	pb "github.com/shin5ok/urlmap-api/pb"
 )
 
 func TestRedirection_GetInfoByUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockClient := mock_urlmap.NewMockRedirectionClient(ctrl)
+
+	// ctx := context.Background()
+
+	redirects := []*pb.RedirectData{}
+	redirects = append(redirects, &pb.RedirectData{Redirect: &pb.RedirectInfo{User: "tako"}})
+	request := &pb.User{User: "tako", NotifyTo: "slack", SlackUrl: "test", Email: ""}
+	mockClient.EXPECT().GetInfoByUser(
+		gomock.Any(),
+		request,
+	).Return(&pb.ArrayRedirectData{Redirects: redirects}, nil)
+
+	// u := &pb.User{User: "tako", NotifyTo: "slack"}
+	testRedirection_GetInfoByUser(t, mockClient)
+
+}
+
+func TestRedirection_GetOrgPath(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockClient := mock_urlmap.NewMockRedirectionClient(ctrl)
+
+	ctx := context.Background()
+
+	path := &pb.RedirectPath{}
+	org := &pb.OrgUrl{}
+	mockClient.EXPECT().GetOrgByPath(
+		ctx,
+		path,
+	).Return(org, nil)
+
+	testRedirection_GetOrgPath(t, mockClient)
+
+}
+func testRedirection_GetOrgPath(t *testing.T, client *mock_urlmap.MockRedirectionClient) {
+	ctx := context.Background()
+	resp, err := client.GetOrgByPath(ctx, &pb.RedirectPath{})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(resp.GetOrg())
+}
+
+func testRedirection_GetInfoByUser(t *testing.T, client *mock_urlmap.MockRedirectionClient) {
+	ctx := context.Background()
+	user := &pb.User{User: "tako", SlackUrl: "test", NotifyTo: "slack"}
+	resp, err := client.GetInfoByUser(ctx, user)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(resp.GetRedirects())
 }
