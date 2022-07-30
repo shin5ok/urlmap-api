4146b6adc1c661593ba0da44b99069e37dcb6949
shin5ok <shin5ok@55mp.com>
2022-07-30 18:46:47 +0900

    add test

--- service/service_test.go
+++ service/service_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -99,4 +100,53 @@ func TestRedirection_PingPongMessage(t *testing.T) {
 		t.Log(resp.GetName())
 	}(t, mockClient)
 
+}
+
+func TestRedirection_SetInfo(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		r   *pb.RedirectData
+	}
+	ctrl := gomock.NewController(t)
+	mockClient := mock_urlmap.NewMockRedirectionClient(ctrl)
+	ctx := context.Background()
+
+	redirectInfo := &pb.RedirectInfo{User: "foo"}
+	mockClient.EXPECT().SetInfo(
+		ctx,
+		&pb.RedirectData{Redirect: redirectInfo},
+	).
+		Return(&pb.OrgUrl{Org: "https://www.google.com/"}, nil)
+
+	tests := []struct {
+		name    string
+		args    args
+		want    *pb.OrgUrl
+		wantErr bool
+	}{
+		{
+			name: "test SetInfo()",
+			want: &pb.OrgUrl{Org: "https://www.google.com/"},
+			args: args{
+				ctx: ctx,
+				r: &pb.RedirectData{
+					Redirect: &pb.RedirectInfo{
+						User: "foo",
+					}}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := mockClient.SetInfo(tt.args.ctx, tt.args.r)
+			t.Log(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Redirection.SetInfo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Redirection.SetInfo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
